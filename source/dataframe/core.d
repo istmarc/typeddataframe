c0ac096aa9cc9be1278e07cd3858723282a3cef8
Marc <mnawman@proton.me>
2025-08-15 23:07:34 +0200

    Add DataFrameSlice for slicing

--- source/dataframe/core.d
+++ source/dataframe/core.d
@@ -5,6 +5,7 @@ import std.variant;
 import std.sumtype;
 import std.container;
 import std.conv;
+import std.typecons;
 
 import mir.ndslice;
 
@@ -53,6 +54,76 @@ Type getColumnType(T : string)() {
 }
 
 /++
+   Column value type.
++/
+alias ColumnValue = SumType!(Slice!(int*), Slice!(long*), Slice!(ulong*),
+   Slice!(float*), Slice!(double*), Slice!(string*));
+
+/++
+Slice fo a data frame.
++/
+class DataFrameSlice(Ts...){
+private:
+   TypedDataFrame!Ts data;
+   ulong[] rowIndices;
+   ulong[] colIndices;
+
+   Tuple!(ulong, ulong) getColumnIndices() {
+      size_t colLength = colIndices.length;
+      size_t colStart = colIndices[0];
+      size_t colEnd = 0;
+      if (colLength == 1) {
+         colEnd = colIndices[0]+1;
+      } else {
+         colEnd = colIndices[1];
+      }
+      return tuple(colStart, colEnd);
+   }
+
+   Tuple!(ulong, ulong) getRowIndices() {
+      size_t rowLength = rowIndices.length;
+      size_t rowStart = rowIndices[0];
+      size_t rowEnd = 0;
+      if (rowLength == 1) {
+         rowEnd = rowStart + 1;
+      } else {
+         rowEnd = rowIndices[1];
+      }
+      return tuple(rowStart, rowEnd);
+   }
+
+public:
+   this(TypedDataFrame!Ts df, ulong[] rows, ulong[] cols) {
+      this.data = df;
+      this.rowIndices = rows;
+      this.colIndices = cols;
+   }
+
+   override string toString(){
+      string str = "DataFrameSlice\n";
+      auto colBounds = getColumnIndices();
+      size_t colStart = colBounds[0];
+      size_t colEnd = colBounds[1];
+      auto rowBounds = getRowIndices();
+      size_t rowStart = rowBounds[0];
+      size_t rowEnd = rowBounds[1];
+
+      for(size_t j = colStart; j < colEnd; j++) {
+         str ~= data.name(j);
+         str ~= ": ";
+         str ~= to!string(data.col(j));
+         if (j + 1 < colEnd) {
+           str ~= "\n";
+         }
+      }
+      return str;
+   }
+
+
+};
+
+
+/++
 Typed data frames, data frames with type information.
 +/
 class TypedDataFrame(Ts...) {
@@ -75,6 +146,7 @@ private:
       return value;
    }
 
+
 public:
    this(string title = "") { this.title = title;}
 
@@ -86,6 +158,11 @@ public:
       }
    }
 
+   void setValue(T)(size_t row, size_t col, T value) {
+      auto values = cast(Slice!(T*)*) colValues[col];
+      (*values)[row] = value;
+   }
+
    void setTitle(string title) {this.title = title;}
 
    void setIndexName(string indexName) {this.indexName = indexName;}
@@ -95,7 +172,7 @@ public:
    string[Ts.length] names() const {return colNames;}
 
    // Set the column at index.
-   void setcol(T)(size_t index, string name, Slice!(T*) values) {
+   void setCol(T)(size_t index, string name, Slice!(T*) values) {
       assert(!hasColName(name));
       // Set the name
       colNames[index] = name;
@@ -108,8 +185,21 @@ public:
       //auto ptr = cast(Slice!(T*)*) colValues[index];
    }
 
+   // Set the column at index from a sym type
+   void setCol(T)(size_t index, string name, ColumnValue values) {
+      assert(!hasColName(name));
+      // Set the name
+      colNames[index] = name;
+      // Set the data
+      Slice!(T*)* valuesPtr = new Slice!(T*)();
+      *valuesPtr = values.get!(Slice!(T*));
+      colValues[index] = cast(void*) valuesPtr;
+      // Set the column type
+      colTypes[index] = getColumnType!T();
+   }
+
    // Set the column at the static Index.
-   void setcol(T, size_t Index)(string name, Slice!(T*) values) {
+   void setCol(T, size_t Index)(string name, Slice!(T*) values) {
       assert(T == Ts[Index]);
       assert(!hasColName(name));
       // Set the name
@@ -125,32 +215,43 @@ public:
 
    // Get the column at index
    Slice!(T*) col(T)(size_t index) const {
-      alias ReturnType = Slice!(T*);
+      //alias ReturnType = Slice!(T*);
       auto ptr = cast(Slice!(T*)*) colValues[index];
       return *ptr;
    }
 
-   // Get the column at static Index
-   auto col(size_t Index)() const {
-      alias T = Ts[Index];
-      auto ptr = cast(Slice!(Ts[Index]*)*) colValues[Index];
-      return *ptr;
+   ColumnValue col(size_t index) const {
+      auto type = colTypes[index];
+      assert(type != Type.None, "Column at " ~ to!string(index) ~ " must have type.");
+      if (type == Type.Int) {
+         auto ptr = cast(Slice!(int*)*) colValues[index];
+         return ColumnValue(*ptr);
+      } else if (type == Type.Float) {
+         auto ptr = cast(Slice!(float*)*) colValues[index];
+         return ColumnValue(*ptr);
+      } else if (type == Type.Double) {
+         auto ptr = cast(Slice!(double*)*) colValues[index];
+         return ColumnValue(*ptr);
+      } else {
+         auto ptr = cast(Slice!(string*)*) colValues[index];
+         return ColumnValue(*ptr);
+      }
    }
 
    // Create a new data frame by adding a column at the end.
-   TypedDataFrame!(Ts, T) addcol(T)(string name, Slice!(T*) values) const {
+   TypedDataFrame!(Ts, T) addCol(T)(string name, Slice!(T*) values) const {
       auto df = new TypedDataFrame!(Ts, T)();
       // Copy data
       static foreach(i; 0..Ts.length) {
-         df.setcol(i, this.name(i), this.col!i());
+         df.setCol!(Ts[i])(i, this.name(i), this.col(i));
       }
       // Copy new column
-      df.setcol(Ts.length, name, values);
+      df.setCol!T(Ts.length, name, values);
       return df;
    }
 
    // Create a new data frame by adding a column at index Index.
-   auto addcol(T, size_t Index)(string name, Slice!(T*) values) const {
+   auto addCol(size_t Index, T)(string name, Slice!(T*) values) const {
       assert(!this.hasColName(name));
       static if (Index == 0) {
          auto df = new TypedDataFrame!(T, Ts)();
@@ -160,19 +261,55 @@ public:
       // Copy data
       static if (Index > 0) {
          static foreach(i; 0..Index) {
-            df.setcol(i, this.name(i), this.col!i());
+            df.setCol!(Ts[i])(i, this.name(i), this.col(i));
          }
       }
       // Copy new column
-      df.setcol!T(Index, name, values);
+      df.setCol!T(Index, name, values);
 
       // Copy remaining data
       static if (Ts.length >= Index) {
          static foreach(i; Index..Ts.length) {
-            df.setcol(i+1, this.name(i), this.col!(i)());
+            df.setCol!(Ts[i])(i+1, this.name(i), this.col(i));
          }
       }
       return df;
+   }
+
+
+   // Overload .opIndex!T(index) to get the column at index i
+   ref Slice!(T*) opIndex(T)(size_t index) const {
+      alias SliceType = Slice!(T*);
+      auto values = cast(SliceType*) colValues[index];
+      return *values;
+   }
+
+   // Overload .opIndex!T(i, j)
+   ref T opIndex(T)(size_t i, size_t j) const {
+      alias SliceType = Slice!(T*);
+      auto values = cast(SliceType*) colValues[j];
+      return (*values)[i];
+   }
+
+   // Overload [[], ]
+   DataFrameSlice!Ts opIndex(size_t[] i, size_t j) {
+      DataFrameSlice!Ts df = new DataFrameSlice!Ts(this, i, [j]);
+      return df;
+   }
+
+   // Overload [ , []]
+   DataFrameSlice!Ts opIndex(size_t i, size_t[] j) {
+      DataFrameSlice!Ts df = new DataFrameSlice!Ts(this, [i], j);
+      return df;
+   }
+
+   DataFrameSlice!Ts opIndex(size_t[] i, size_t[] j) {
+      DataFrameSlice!Ts df = new DataFrameSlice!Ts(this, i, j);
+      return df;
+   }
+
+   size_t[2] opSlice(size_t dim)(size_t start, size_t end) {
+      return [start, end];
    }
 
    override string toString(){
@@ -188,7 +325,7 @@ public:
       static foreach(j; 0..cols) {
          str ~= name(j);
          str ~= ": ";
-         str ~= to!string(this.col!j());
+         str ~= to!string(this.col(j));
          static if (j + 1 < cols) {
             str ~= "\n";
          }
@@ -197,6 +334,9 @@ public:
    }
 }
 
+/++
+DataFrame alias for TypedDataFrame
++/
 alias DataFrame(Ts...) = TypedDataFrame!Ts;
 
 
--- test/source/app.d
+++ test/source/app.d
@@ -8,17 +8,17 @@ import mir.ndslice;
 void main()
 {
    auto df = new TypedDataFrame!(float, double, string)("DataFrame DF");
-   df.setcol(0, "a", zeros!float(10));
-   df.setcol(1, "b", ones!double(10));
-   df.setcol(2, "c", ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"].sliced(10));
+   df.setCol(0, "a", zeros!float(10));
+   df.setCol(1, "b", ones!double(10));
+   df.setCol(2, "c", ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"].sliced(10));
 
-   auto col0 = df.col!0();
+   auto col0 = df.col(0);
    writeln(col0);
 
    auto col1 = df.col!double(1);
    writeln(col1);
 
-   auto col11 = df.col!1();
+   auto col11 = df.col(1);
    writeln(col11);
 
    writeln(df);
@@ -31,29 +31,54 @@ void main()
 
    {
       writeln("Add column at the end");
-      auto newdf = df.addcol!float("d", x);
+      auto newdf = df.addCol!float("d", x);
       newdf.setTitle("DataFrame NEW DF");
       writeln(newdf);
    }
 
    {
       writeln("Add column at the begining");
-      auto newdf = df.addcol!(float, 0)("d", x);
+      auto newdf = df.addCol!(0, float)("d", x);
       writeln(newdf);
    }
 
    {
       writeln("Add column in the middle");
-      auto newdf = df.addcol!(float, 1)("d", x);
+      auto newdf = df.addCol!1("d", x);
       writeln(newdf);
    }
 
    {
       auto newdf = new DataFrame!(float, float)();
       newdf.setTitle("DataFrame using DataFrame");
-      newdf.setcol(0, "a", x);
-      newdf.setcol(1, "b", x);
+      newdf.setCol(0, "a", x);
+      newdf.setCol(1, "b", x);
+      // set value
+      newdf.setValue!float(0, 0, 99.0f);
+      // Get value [i,j]
+      float value = newdf.opIndex!float(0, 0);
+      writeln("value[0,0] = ", value);
       writeln(newdf);
+      writeln("Get the first column");
+      writeln(newdf.opIndex!float(0));
+
+      {
+         writeln("Slices [0, 0...2]");
+         auto slice = newdf[0, 0..2];
+         writeln(slice);
+      }
+
+      {
+         writeln("SLices [0..10, 0]");
+         auto slice = newdf[0..10, 0];
+         writeln(slice);
+      }
+
+      {
+         writeln("Slices [0..10, 0..2]");
+         auto slice = newdf[0..10, 0..2];
+         writeln(slice);
+      }
    }
 
 }
